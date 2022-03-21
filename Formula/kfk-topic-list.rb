479784453253c65550bf7dd946db514b61f88cf9
jreleaserbot <jreleaser@kordamp.org>
2022-03-21 19:58:14 +0000

    kfk-topic-list cli-topic-list-v0.1.4

--- Formula/kfk-topic-list.rb
+++ Formula/kfk-topic-list.rb
@@ -1,17 +1,17 @@
-# Generated with JReleaser 1.0.0-M2 at 2022-03-21T19:44:00.670945762Z
+# Generated with JReleaser 1.0.0-M2 at 2022-03-21T19:58:03.187097813Z
 class KfkTopicList < Formula
   desc "Kafka: CLI: Topic List: Expand Kafka topic listing with Offsets and more."
   homepage "https://github.com/jeqo/poc-apache-kafka"
-  version "0.1.3"
+  version "0.1.4"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/jeqo/poc-apache-kafka/releases/download/cli-topic-list-v0.1.3/kfk-topic-list-0.1.3-linux-x86_64.zip"
-    sha256 "bda8a9faa7adade73f0162c475c3c2150aaf8eae076a83aba657cd346f4d1a8e"
+    url "https://github.com/jeqo/poc-apache-kafka/releases/download/cli-topic-list-v0.1.4/kfk-topic-list-0.1.4-linux-x86_64.zip"
+    sha256 "bdfce3042cff35a3be5f89a0dad523c4a9cd5fbdba8396247e2ad30fc99f2462"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/jeqo/poc-apache-kafka/releases/download/cli-topic-list-v0.1.3/kfk-topic-list-0.1.3-osx-x86_64.zip"
-    sha256 "dda049d84e0bcb8788098d8bd08603df8cb4c44893aa13c02b8fc7b868cb3c2c"
+    url "https://github.com/jeqo/poc-apache-kafka/releases/download/cli-topic-list-v0.1.4/kfk-topic-list-0.1.4-osx-x86_64.zip"
+    sha256 "03049146434f2c26635b38403647043faf93a15322812efeb2e585e1ebda1638"
   end
 
 
@@ -22,6 +22,6 @@ class KfkTopicList < Formula
 
   test do
     output = shell_output("#{bin}/kfk-topic-list --version")
-    assert_match "0.1.3", output
+    assert_match "0.1.4", output
   end
 end
