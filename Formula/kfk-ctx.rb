9d0a08c5ac3b9c36a7222ee3672513de4986d054
jreleaserbot <jreleaser@kordamp.org>
2022-03-29 17:55:33 +0000

    kfk-ctx cli-context-v0.2.8

--- Formula/kfk-ctx.rb
+++ Formula/kfk-ctx.rb
@@ -1,17 +1,17 @@
-# Generated with JReleaser 1.0.0-M2 at 2022-03-28T00:23:45.020937627Z
+# Generated with JReleaser 1.0.0-M2 at 2022-03-29T17:55:20.542076909Z
 class KfkCtx < Formula
   desc "Kafka: CLI: Context: Manage Kafka cluster contexts."
   homepage "https://github.com/jeqo/poc-apache-kafka"
-  version "0.2.7"
+  version "0.2.8"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/jeqo/poc-apache-kafka/releases/download/cli-context-v0.2.7/kfk-ctx-0.2.7-linux-x86_64.zip"
-    sha256 "ff2d3161a37b401edb445fe507ec58bd0d55a7859f777f9de14bdd23a5ea5508"
+    url "https://github.com/jeqo/poc-apache-kafka/releases/download/cli-context-v0.2.8/kfk-ctx-0.2.8-linux-x86_64.zip"
+    sha256 "80b343f0ee0fd79730f145b6f31754817537bdf22558b4cdb33594c9ede53683"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/jeqo/poc-apache-kafka/releases/download/cli-context-v0.2.7/kfk-ctx-0.2.7-osx-x86_64.zip"
-    sha256 "33b0e1e7ac0b7b065cb7ddbdec45224c7aaf80e8407eec638da2811e480f33dc"
+    url "https://github.com/jeqo/poc-apache-kafka/releases/download/cli-context-v0.2.8/kfk-ctx-0.2.8-osx-x86_64.zip"
+    sha256 "3b059ff1907ef09aaf6b49ce389ebf4db44025b7c04c62c5f88b318501d4312a"
   end
 
 
@@ -22,6 +22,6 @@ class KfkCtx < Formula
 
   test do
     output = shell_output("#{bin}/kfk-ctx --version")
-    assert_match "0.2.7", output
+    assert_match "0.2.8", output
   end
 end
