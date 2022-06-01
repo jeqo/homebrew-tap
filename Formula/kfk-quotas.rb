302e085e99da76342d6417715795c9b689cf62c5
jreleaserbot <jreleaser@kordamp.org>
2022-06-01 01:07:22 +0000

    kfk-quotas cli-quotas-v0.1.1

--- Formula/kfk-quotas.rb
+++ Formula/kfk-quotas.rb
@@ -1,17 +1,17 @@
-# Generated with JReleaser 1.0.0-M2 at 2022-04-14T15:04:14.598481959Z
+# Generated with JReleaser 1.0.0-M2 at 2022-06-01T01:07:09.545672875Z
 class KfkQuotas < Formula
   desc "Kafka: CLI: Quotas: Manage Kafka Quotas."
-  homepage "https://github.com/jeqo/poc-apache-kafka"
-  version "0.1.0"
+  homepage "https://github.com/jeqo/kafka-cli/tree/quotas"
+  version "0.1.1"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/jeqo/poc-apache-kafka/releases/download/cli-quotas-v0.1.0/kfk-quotas-0.1.0-linux-x86_64.zip"
-    sha256 "f4ca1af8741bc2a15ca13da1694c2d435dfa30df4508a12ae448ba65c9d2fac7"
+    url "https://github.com/jeqo/kafka-cli/releases/download/cli-quotas-v0.1.1/kfk-quotas-0.1.1-linux-x86_64.zip"
+    sha256 "56dfa8f24fefddaf17092e5662abde8493db8f3448d61aee62226de22c685274"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/jeqo/poc-apache-kafka/releases/download/cli-quotas-v0.1.0/kfk-quotas-0.1.0-osx-x86_64.zip"
-    sha256 "d1428a90c95212a63f8311d6961dd161a9163050a47ad82a71b64e554b699052"
+    url "https://github.com/jeqo/kafka-cli/releases/download/cli-quotas-v0.1.1/kfk-quotas-0.1.1-osx-x86_64.zip"
+    sha256 "6035fe0c85bb870c7476ca02a254d48470b788a6c40a938c5e509b8c0c1b1325"
   end
 
 
@@ -22,6 +22,6 @@ class KfkQuotas < Formula
 
   test do
     output = shell_output("#{bin}/kfk-quotas --version")
-    assert_match "0.1.0", output
+    assert_match "0.1.1", output
   end
 end
