483e03dba80eb72d429d826b49d856027959a201
jreleaserbot <jreleaser@kordamp.org>
2022-06-02 01:04:56 +0000

    kfk-cluster-state cli-cluster-state-v0.3.0

--- Formula/kfk-cluster-state.rb
+++ Formula/kfk-cluster-state.rb
@@ -1,17 +1,17 @@
-# Generated with JReleaser 1.0.0-M2 at 2022-06-01T01:35:32.033390797Z
+# Generated with JReleaser 1.0.0-M2 at 2022-06-02T01:04:44.629395287Z
 class KfkClusterState < Formula
   desc "Kafka: CLI: Topic List: Expand Kafka topic listing with Offsets and more."
   homepage "https://github.com/jeqo/kafka-cli"
-  version "0.2.3"
+  version "0.3.0"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/jeqo/kafka-cli/releases/download/cli-cluster-state-v0.2.3/kfk-cluster-state-0.2.3-linux-x86_64.zip"
-    sha256 "4387c9dcffb772039987e79923bc39895622d59b327812c65c4f8e5a53e4497e"
+    url "https://github.com/jeqo/kafka-cli/releases/download/cli-cluster-state-v0.3.0/kfk-cluster-state-0.3.0-linux-x86_64.zip"
+    sha256 "921a2174d95d0483630f864cd6daf15cb869fc16f551cf364c409f934ff62bca"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/jeqo/kafka-cli/releases/download/cli-cluster-state-v0.2.3/kfk-cluster-state-0.2.3-osx-x86_64.zip"
-    sha256 "87e351aa8efebdd2953a99c82105929b4f753197af209a999983b9113fb213af"
+    url "https://github.com/jeqo/kafka-cli/releases/download/cli-cluster-state-v0.3.0/kfk-cluster-state-0.3.0-osx-x86_64.zip"
+    sha256 "d6ab50dd278102ae8023458fd3f38a02eb0f17c071b7f04e7e458a18fc6e8d47"
   end
 
 
@@ -22,6 +22,6 @@ class KfkClusterState < Formula
 
   test do
     output = shell_output("#{bin}/kfk-cluster-state --version")
-    assert_match "0.2.3", output
+    assert_match "0.3.0", output
   end
 end
